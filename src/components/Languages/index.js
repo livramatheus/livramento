9034aed84c3c0e30e92f4b7efac64ac10560bc0c
livramateus <livra.matheus@gmail.com>
2021-11-02 15:23:32 -0300

    Improved Languages component visual

--- src/components/Languages/index.js
+++ src/components/Languages/index.js
@@ -1,31 +1,41 @@
 import LanguageItem from "./LanguageItem";
+import Aos from "aos";
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 export default function Languages() {
-  const langs = [
-    {
-      language: "Portuguese",
-      description: ["Native"],
-      percentage: 100
-    },
-    {
-      language: "English",
-      description: [
-        "Excellent writing",
-        "Excellent reading",
-        "Good listening",
-        "Reasonable speaking"
-      ],
-      percentage: 90
-    }
-  ];
 
-  return (
-    <article className="languages">
-      <h2>Languages I speak</h2>
-      
-      {langs.map((itm, id) => {
-        return <LanguageItem {...itm} key={id} />;
-      })}
-    </article>
-  );
+    useEffect(() => {
+        Aos.init({
+            duration: 1000
+        });
+    }, []);
+
+    const langs = [
+        {
+            language: "Portuguese",
+            description: ["Native"],
+            percentage: 100
+        },
+        {
+            language: "English",
+            description: [
+                "Excellent writing",
+                "Excellent reading",
+                "Good listening",
+                "Reasonable speaking"
+            ],
+            percentage: 90
+        }
+    ];
+
+    return (
+        <article className="languages" data-aos="fade-up" data-aos-once="true">
+            <h2>Languages I speak</h2>
+
+            {langs.map((itm, id) => {
+                return <LanguageItem {...itm} key={id} />;
+            })}
+        </article>
+    );
 }
