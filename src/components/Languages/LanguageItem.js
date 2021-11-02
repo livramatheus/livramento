a81ee7a4aada6aa42fd4be74e67d77da3d34d42b
livramateus <livra.matheus@gmail.com>
2021-11-02 10:12:56 -0300

    Improved Languages component visual

--- src/components/Languages/LanguageItem.js
+++ src/components/Languages/LanguageItem.js
@@ -1,25 +1,57 @@
 import { Line } from "rc-progress";
+import { FaInfoCircle } from 'react-icons/fa';
+import { Popover } from '@material-ui/core';
+import { useState } from "react";
 
 export default function LanguageItem(props) {
   const { language, description, percentage } = props;
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? `popover-${language}` : undefined;
+
   return (
     <div className="language-item">
-      <div className="language-desc">
-        <h2>{language}</h2>
-        
-        <div className="desc-itm">
-          {description.map((e, id) => {
-            return <p key={id}>{e}</p>;
-          })}
+      <div className="language-item-header">
+        <div className="lang-name">
+          <span>{language}</span>
+          <FaInfoCircle className="lang-name-info" onClick={handleClick} aria-describedby={id} />
+          <Popover
+            id={id}
+            open={open}
+            onClose={handleClose}
+            anchorEl={anchorEl}
+            classes={{paper: 'popover-style'}}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            {description.map((e, id) => {
+              return <p key={id}>{e}</p>;
+            })}
+          </Popover>
         </div>
+        <div className="lang-percent">{`${percentage / 10}/10`}</div>
       </div>
-      <div className="language-data">
-        <h2>{`${percentage / 10}/10`}</h2>
+      <div>
         <Line
           percent={percentage}
-          strokeWidth="4"
-          trailWidth="4"
+          strokeWidth="1"
+          trailWidth="1"
           strokeColor="var(--text1)"
           trailColor="var(--blue3)"
           strokeLinecap="round"
