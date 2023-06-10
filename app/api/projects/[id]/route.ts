ea0dbc4e2cbd2279d0d33965b541bb7bd460be94
livramateus <livra.matheus@gmail.com>
2023-06-10 09:30:12 -0300

    feat: develop project routes

---
+++ app/api/projects/[id]/route.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+import IProject from "../../../interfaces/Project";
+
+interface IParams {
+  params: {
+    id: string;
+  }
+}
+
+export const GET = async (request: Request, params: IParams) => {
+  const id = params.params.id;
+  
+  const jsonDirectory = path.join(process.cwd(), 'data');
+  const fileContents = await fs.readFile(jsonDirectory + '/projects.json', 'utf8');
+  const projects = JSON.parse(fileContents).projectList;
+  
+  const project = projects.find((p: IProject) => p.id == parseInt(id));
+
+  if (project) {
+    return new Response(JSON.stringify(project), {
+      status: 200
+    });
+  }
+
+  return new Response("Project not found", {
+    status: 404
+  });
+}
---
+++ app/interfaces/Project.ts
@@ -0,0 +1,9 @@
+export default interface IProject {
+  id: number;
+  title: string;
+  launchdate: string;
+  abstract: string;
+  description: string;
+  technologies: string
+  gitaddr: string;
+}
