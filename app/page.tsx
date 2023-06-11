05e88c799f2dc8182fcf7884d18ff1d2f439aff6
livramateus <livra.matheus@gmail.com>
2023-06-11 13:31:00 -0300

    fix: add proper page title to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import AboutMe from "./components/AboutMe";
 import Greetings from "./components/Greetings";
 
+export const metadata = {
+  title: "Welcome - Livramento.dev",
+  description: 'Matheus do Livramento - Back-end software engineer',
+}
+
 export default function Home() {
   return (
     <>
