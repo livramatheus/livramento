bb8eac39e97df004787a67ec107d3bb29fa3ffc9
livramateus <livra.matheus@gmail.com>
2023-06-25 10:46:53 -0300

    feat: improve tag hierarchy and tweak spacing

--- app/portfolio/[id]/page.tsx
+++ app/portfolio/[id]/page.tsx
@@ -31,23 +31,25 @@ const PortfolioItem = (props: IProps) => {
     <>
       {
         projectDetails && (
-          <section className="flex flex-col gap-10">
+          <section className="flex flex-col gap-8">
             <div>
               <SectionTitle title={projectDetails.title} align="left" />
               <span className="text-zinc-600">Launched {format(new Date(projectDetails.launchdate), "PPPP")}</span>
             </div>
             
-            <div className="bg-zinc-800 text-zinc-200 px-8 py-6 border-l-8 border-zinc-400">
-              {projectDetails.abstract}
+            <div className="flex flex-col gap-5">
+              <div className="bg-zinc-800 text-zinc-200 px-8 py-6 border-l-8 border-zinc-400">
+                {projectDetails.abstract}
+              </div>
+              
+              <div className="flex gap-3">
+                {
+                  projectDetails.repourl.map((r, k) => <RepoBtn key={k} title={r.title} repoUrl={r.url} />)
+                }
+              </div>
             </div>
             
-            <div className="flex gap-3">
-              {
-                projectDetails.repourl.map((r, k) => <RepoBtn key={k} title={r.title} repoUrl={r.url} />)
-              }
-            </div>
-            
-            <div dangerouslySetInnerHTML={{ __html: projectDetails.description }} />
+            <article className="portfolio-item" dangerouslySetInnerHTML={{ __html: projectDetails.description }} />
           </section>
         )
       }
