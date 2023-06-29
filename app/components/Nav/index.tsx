1792f6f7062a7ee8c0b9c6edf11a68f915d69569
livramateus <livra.matheus@gmail.com>
2023-06-29 20:51:12 -0300

    fix: small navbar fixes

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -2,15 +2,14 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between text-zinc-200">
-      <Link href="/" className="flex gap-3">
+    <nav className="flex justify-between items-center text-zinc-200">
+      <Link href="/" className="flex gap-3 items-center text-lg">
         <span>☕</span>
         <span className="font-bold">Matheus do Livramento</span>
       </Link>
 
       <div className="flex gap-10">
         <Link className="hover:underline" href="/portfolio">Portfolio</Link>
-        <Link className="hover:underline" href="/learning">Learning</Link>
         <Link className="hover:underline" href="/about">About</Link>
         <Link className="hover:underline" href="/contact">Contact</Link>
       </div>
