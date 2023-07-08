d9c7842dfee2b41c753b1d2218b5065e21b64480
livramateus <livra.matheus@gmail.com>
2023-07-07 21:50:28 -0300

    feat: develop projects preview

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 import AboutMe from "./components/AboutMe";
 import Greetings from "./components/Greetings";
+import ProjectsPreview from "./components/ProjectsPreview";
+import IProject from "./interfaces/Project";
 
 export const metadata = {
   title: "Welcome - Livramento.dev",
   description: 'Matheus do Livramento - Back-end software engineer',
 }
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch(`${process.env.SERVER_URL}/api/projects`);
+  const jsonList = await response.json();
+  let projects: IProject[] = jsonList.projectList;
+  projects = projects.reverse();
+  projects = projects.slice(0, 3);
+
   return (
     <>
       <Greetings/>
 
       <AboutMe/>
+
+      <ProjectsPreview projects={projects} />
     </>
   )
 }
