73c80978738b2887d0f4e8563df6f33bbfaa27cc
livramateus <livra.matheus@gmail.com>
2023-07-09 20:26:26 -0300

    fix: check potential undefined props

--- components/PortfolioList/index.tsx
+++ components/PortfolioList/index.tsx
@@ -6,17 +6,19 @@ interface IProps {
 }
 
 const PortfolioList = ({ projects }: IProps) => {
-  return (
-    projects.map((p, i) => {
-      return (
-        <div key={i} className="flex flex-col gap-3 bg-zinc-800 py-8 px-7 rounded-lg border-l-8 max-md:w-full w-9/12 border-zinc-400 hover:border-blue-400">
-          <Link href={`/portfolio/${p.id}`} className="text-zinc-200 text-3xl font-bold hover:underline">{p.title}</Link>
-          <div className="text-zinc-200">{p.abstract}</div>
-          <div className="text-zinc-600">Technologies: {p.technologies}</div>
-        </div>
-      );
-    })
-  )
+  if (projects !== undefined) {
+    return (
+      projects.map((p, i) => {
+        return (
+          <div key={i} className="flex flex-col gap-3 bg-zinc-800 py-8 px-7 rounded-lg border-l-8 max-md:w-full w-9/12 border-zinc-400 hover:border-blue-400">
+            <Link href={`/portfolio/${p.id}`} className="text-zinc-200 text-3xl font-bold hover:underline">{p.title}</Link>
+            <div className="text-zinc-200">{p.abstract}</div>
+            <div className="text-zinc-600">Technologies: {p.technologies}</div>
+          </div>
+        );
+      })
+    )
+  }
 }
 
 export default PortfolioList;
--- components/ProjectsPreview/index.tsx
+++ components/ProjectsPreview/index.tsx
@@ -9,28 +9,30 @@ interface IProps {
 const ProjectsPreview = (props: IProps) => {
   const { projects } = props;
 
-  return (
-    <section className="flex flex-col max-md:gap-10 gap-20 ">
-      <SectionTitle title="Latest Projects" />
-
-      <div className="grid grid-cols-2 gap-4">
-        {
-          projects.map((p, k) => {
-            return (
-              <div
-                key={k}
-                className={`flex flex-col gap-4 justify-center bg-zinc-800 py-8 px-7 rounded-lg border-l-8 border-zinc-400 hover:border-blue-400 ${k === 2 ? "col-span-2" : ""}`}
-              >
-                <Link href={`/portfolio/${p.id}`} className="text-zinc-200 text-3xl font-bold hover:underline">{p.title}</Link>
-                <div className="text-zinc-200">{p.abstract}</div>
-                <div className="text-zinc-500">Technologies: {p.technologies}</div>
-              </div>
-            )
-          })
-        }
-      </div>
-    </section>
-  );
+  if (projects !== undefined) {
+    return (
+      <section className="flex flex-col max-md:gap-10 gap-20 ">
+        <SectionTitle title="Latest Projects" />
+  
+        <div className="grid grid-cols-2 gap-4">
+          {
+            projects.map((p, k) => {
+              return (
+                <div
+                  key={k}
+                  className={`flex flex-col gap-4 justify-center bg-zinc-800 py-8 px-7 rounded-lg border-l-8 border-zinc-400 hover:border-blue-400 ${k === 2 ? "col-span-2" : ""}`}
+                >
+                  <Link href={`/portfolio/${p.id}`} className="text-zinc-200 text-3xl font-bold hover:underline">{p.title}</Link>
+                  <div className="text-zinc-200">{p.abstract}</div>
+                  <div className="text-zinc-500">Technologies: {p.technologies}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </section>
+    );
+  }
 }
 
 export default ProjectsPreview;
